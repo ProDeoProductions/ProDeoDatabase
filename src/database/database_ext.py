2cc19b42007c0d343c2c21706331a882b22823c1
Zowiezo101 <91847374+Zowiezo101@users.noreply.github.com>
2023-09-22 13:11:16 +0200

    Now able to backup the local and external database, and push the local backup to the external database

--- src/database/database_ext.py
+++ src/database/database_ext.py
@@ -8,10 +8,16 @@ from mysql.connector import errorcode
 class DatabaseExt:
     def __init__(self):
         self.conn = mysql.connector
+
+        # External database
         self.dba_host = ""
         self.dba_username = ""
         self.dba_password = ""
         self.dba_database = ""
+
+        # Local database
+        self.db_username = src.settings.db_username
+        self.db_password = src.settings.db_password
 
         if hasattr(src.settings, "dba_host"):
             self.dba_host = src.settings.dba_host
@@ -37,7 +43,32 @@ class DatabaseExt:
         conn = self.connect_ext()
         if conn is not None:
             # We are connected, start by making a backup
-            self.backup_ext(conn)
+            self.backup_ext()
+
+            # Time to backup the local database as well
+            self.backup_local()
+
+            print("Updating the external database with the stored 'bible.sql' file")
+            # Backup is created, time to update the database with the current state of the local database
+            cursor = conn.cursor()
+
+            # Backup of the local database
+            file = open("files/sql/bible.sql", encoding="UTF8")
+            sql = file.read()
+
+            # We need to change the database name to the name of the external database
+            sql = sql.replace("bible", self.dba_database)
+
+            # This is an annoying issue.. The local database uses the "utf8mb4_0900_ai_ci" collation
+            # by default, but it doesn't exist in the version the external database uses
+            sql = sql.replace("utf8mb4_0900_ai_ci", "utf8mb4_general_ci")
+
+            cursor.execute(sql, multi=True)
+
+            # Close the cursor again
+            cursor.close()
+
+            print("Update finished")
 
         conn.close()
         return
@@ -60,9 +91,16 @@ class DatabaseExt:
 
         return conn
 
-    def backup_ext(self, conn):
-        print("Generating backup in files/sql/")
+    def backup_local(self):
+        print("Exporting local database in files/sql/bible.sql")
+        os.system('mysqldump --host=localhost --port=3306 --default-character-set=utf8 '
+                  '--user=%s -p%s --protocol=tcp --skip-triggers "bible" > files/sql/backup.sql' %
+                  (self.db_username, self.db_password))
+        print("Export finished\n")
+
+    def backup_ext(self):
+        print("Generating backup in files/sql/backup.sql")
         os.system('mysqldump --host=%s --port=3306 --default-character-set=utf8 '
                   '--user=%s -p%s --protocol=tcp --skip-triggers "%s" > files/sql/backup.sql' %
                   (self.dba_host, self.dba_username, self.dba_password, self.dba_database))
-        print("Backup created")
+        print("Backup created\n")
